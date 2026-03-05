ac3073d983bd49c2d76be622d2712416e358e35c
LUTFI IMAM TFA <lutfiimam81@gmail.com>
2026-03-05 06:54:02 +0000

    Data Tidak Sinkron: Anak magang yang memilih "Pra-Probation" salah dikat

--- src/firebase/firestore/use-doc.tsx
+++ src/firebase/firestore/use-doc.tsx
@@ -41,7 +41,7 @@ export interface UseDocResult<T> {
  * @returns {UseDocResult<T>} Object with data, isLoading, error.
  */
 export function useDoc<T = any>(
-  memoizedDocRef: DocumentReference<DocumentData> | null | undefined,
+  memoizedDocRef: (DocumentReference<DocumentData> & {__memo?: boolean}) | null | undefined,
 ): UseDocResult<T> {
   type StateDataType = WithId<T> | null;
 
@@ -107,6 +107,10 @@ export function useDoc<T = any>(
 
     return () => unsubscribe();
   }, [memoizedDocRef]);
+
+  if(memoizedDocRef && !memoizedDocRef.__memo) {
+    throw new Error(memoizedDocRef + ' was not properly memoized using useMemoFirebase');
+  }
 
   return { data, isLoading, error, mutate: fetchData };
 }
